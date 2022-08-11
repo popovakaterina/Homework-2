15952da68b1587d96e5ba96b14b9b337fcb6c93f
Popova Ekaterina <popova_katerina97@mail.ru>
2022-08-12 00:37:55 +0500

    Task 10

---
+++ Task10/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и 
+// на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Print your number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string strnumber = Convert.ToString(number);
+Console.Write("Second figure is: ");
+Console.Write(strnumber[1]);
