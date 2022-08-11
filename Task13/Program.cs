19309e59f1dd675072af74f8da135791231d6526
Popova Ekaterina <popova_katerina97@mail.ru>
2022-08-12 00:46:28 +0500

    Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,13 @@
+﻿//  Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+Console.WriteLine("Print your number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string strnumber = Convert.ToString(number);
+if (strnumber.Length >2)
+{
+    Console.Write("Third element is: ");
+    Console.Write(strnumber[2]);
+}
+else
+{
+    Console.Write("This number is shorter than tree elements!");
+}
